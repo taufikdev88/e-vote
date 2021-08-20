d699dc9dac4410738b1b6e7bdcd1523be5c155a4
Taufik Hidayat <taufikdev88@gmail.com>
2021-08-20 21:21:38 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'web.student.pens.ac.id';
-$db['default']['username'] = 'mekaevote';
-$db['default']['password'] = 'Doxijuj9#';
-$db['default']['database'] = 'mekaevote';
+$db['default']['hostname'] = '';
+$db['default']['username'] = '';
+$db['default']['password'] = '';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
